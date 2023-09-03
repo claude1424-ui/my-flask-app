d9704d2de9d2077c478c8854699d0941a40b2965
claude1424-ui <93033246+claude1424-ui@users.noreply.github.com>
2023-09-03 16:08:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
 @app.route('/')
 def chat():
     return """
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Chat GPT</title>
+        <title>Chat Flask</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
                 background-color: #f0f0f0;
                 margin: 0;
                 padding: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-            }
-
-            .container {
-                max-width: 600px;
-                width: 100%;
-                padding: 20px;
-                background-color: #fff;
-                border: 1px solid #ccc;
-                border-radius: 5px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             }
 
             h1 {
@@ -34,68 +25,63 @@ def chat():
                 padding: 10px;
             }
 
-            #chat {
-                max-height: 400px;
-                overflow-y: scroll;
-                padding: 10px;
+            #chat-container {
+                max-width: 600px;
+                margin: 20px auto;
+                padding: 20px;
+                background-color: #fff;
                 border: 1px solid #ccc;
                 border-radius: 5px;
-                background-color: #f9f9f9;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             }
 
-            p {
-                margin: 5px 0;
+            #chat {
+                margin-bottom: 10px;
+                padding: 10px;
+                background-color: #f9f9f9;
+                border: 1px solid #ddd;
+                border-radius: 5px;
+                min-height: 100px;
+                max-height: 300px;
+                overflow-y: scroll;
             }
 
             input[type="text"] {
                 width: 80%;
-                padding: 10px;
+                padding: 5px;
                 border: 1px solid #ccc;
                 border-radius: 3px;
-                margin-right: 10px;
             }
 
             button {
-                padding: 10px 15px;
+                padding: 5px 10px;
                 background-color: #333;
                 color: #fff;
                 border: none;
                 border-radius: 3px;
                 cursor: pointer;
             }
-
-            .user-message {
-                text-align: right;
-                color: #333;
-                margin-right: 10px;
-                background-color: #d3e0dc;
-                border-radius: 5px;
-                padding: 5px;
-            }
-
-            .gpt-message {
-                text-align: left;
-                color: #0066cc;
-                margin-left: 10px;
-                background-color: #e0ecff;
-                border-radius: 5px;
-                padding: 5px;
-            }
         </style>
     </head>
     <body>
-        <div class="container">
-            <h1>Chat GPT</h1>
+        <h1>Chat Flask</h1>
+        <div id="chat-container">
             <div id="chat"></div>
-            <div class="input-container">
-                <input type="text" id="message" placeholder="Entrez votre message">
-                <button id="send">Envoyer</button>
-            </div>
+            <input type="text" id="message" placeholder="Entrez votre message">
+            <button id="send">Envoyer</button>
         </div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js"></script>
         <script>
-            // Le reste de votre code JavaScript ici...
+            document.getElementById('send').addEventListener('click', function() {
+                var messageInput = document.getElementById('message');
+                var message = messageInput.value;
+                // Vous pouvez traiter le message ici et envoyer une réponse depuis le serveur Flask
+                // Par exemple, en utilisant une requête AJAX pour envoyer le message au serveur
+                messageInput.value = ''; // Effacez le champ de texte après avoir envoyé le message
+            });
         </script>
     </body>
     </html>
     """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
