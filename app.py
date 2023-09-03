5d034bdbc33b1412d96f0f3dda0c78639e5ae99e
claude1424-ui <93033246+claude1424-ui@users.noreply.github.com>
2023-09-03 15:54:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 import os
-import subprocess
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def chat():
-    data = request.get_json()
-    user_message = data['message'].lower()
-    
-    # Ajoutez ici votre logique de chat GPT en réponse au message de l'utilisateur.
-    # Vous pouvez utiliser les bibliothèques nécessaires pour votre logique de chat.
-
-    # Exemple de réponse :
-    response = {"message": "Ceci est la réponse de GPT."}
-
-    return jsonify(response)
+    return "Bienvenue sur votre application Flask sur Vercel !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
