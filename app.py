07fa8cfda7f0e18d24a88fe04da02a69a73f5e1c
claude1424-ui <claudetitouan24@gmail.com>
2023-09-03 15:49:55 +0200

    Description de vos modifications

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+import os
+import subprocess
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route('/', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_message = data['message'].lower()
+    
+    # Ajoutez ici votre logique de chat GPT en réponse au message de l'utilisateur.
+    # Vous pouvez utiliser les bibliothèques nécessaires pour votre logique de chat.
+
+    # Exemple de réponse :
+    response = {"message": "Ceci est la réponse de GPT."}
+
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
