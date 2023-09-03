a02c5210d9eba4a50fb44f6ccf00bd8375fdf31b
claude1424-ui <93033246+claude1424-ui@users.noreply.github.com>
2023-09-03 15:58:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def chat():
-    return "Bienvenue sur votre application Flask sur Vercel !"
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Chat Flask</title>
+    </head>
+    <body>
+        <h1>Chat Flask</h1>
+        <div id="chat-container">
+            <div id="chat"></div>
+            <input type="text" id="message" placeholder="Entrez votre message">
+            <button id="send">Envoyer</button>
+        </div>
+        <script>
+            document.getElementById('send').addEventListener('click', function() {
+                var messageInput = document.getElementById('message');
+                var message = messageInput.value;
+                // Vous pouvez traiter le message ici et envoyer une réponse depuis le serveur Flask
+                // Par exemple, en utilisant une requête AJAX pour envoyer le message au serveur
+                messageInput.value = ''; // Effacez le champ de texte après avoir envoyé le message
+            });
+        </script>
+    </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
